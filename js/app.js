b079466294bf21226634c02c6f52486498682854
John Warren <JWarren360@gmail.com>
2015-11-10 22:31:14 -0600

    Refactor Code

--- js/app.js
+++ js/app.js
@@ -15,28 +15,35 @@ function winLose() {
             player.resetPos();
         }
     }
-    //Test if player reaches water
-    //Win test
-    if (player.y < 50) {
-        console.log("You Win!!"); //Debug Only
-        //Add to win score and post to html tag
-        player.win++;
-        winning.innerHTML = player.win;
-        player.resetPos();
-    }
+
 }
 
 // Enemies player must avoid
 var Enemy = function() {
+    //intital X corinate completely random
+    this.x = (Math.random() * 505) * (Math.random() < 0.5 ? -1 : 1);
+    switch(Math.floor((Math.random() * 3) + 1)) {
+        case 1 : this.y = 61; break;
+        case 2 : this.y = 145; break;
+        case 3 : this.y=229; break;
+    }
+    this.left = this.x + 3;
+    this.right = this.x + 99;
+    this.top = this.y + 77;
+    this.bottom = this.y + 140;
+    this.speed = 100; //regular 100
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
     //Desides what path Enemy will take randomly
     this.yLocation = function() {
-        var y = Math.floor((Math.random() * 3) + 1);
-        if (y == 1) {
-            this.y = 61;
-        } else if (y == 2) {
-            this.y = 145;
-        } else {
-            this.y = 229;
+        switch(Math.floor((Math.random() * 3) + 1)) {
+            case 1 : this.y = 61; break;
+            case 2 : this.y = 145; break;
+            case 3 : this.y=229; break;
         }
     };
     //sets Enemy X location randomly but off screen
@@ -54,39 +61,34 @@ var Enemy = function() {
         this.yLocation();
         this.resetBox();
     };
-    this.speed = 100; //regular 100
-    this.sprite = 'images/enemy-bug.png';
-    this.resetPos();
-    //intital X corinate completely random
-    this.x = (Math.random() * 505) * (Math.random() < 0.5 ? -1 : 1);
-
-};
-
-// Update the enemy's position
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
     this.x += this.speed * dt;
     //resets bugs position after crossing game board
     if (this.x > 500) {
         this.resetPos();
     }
     this.resetBox();
-    winLose();
+    //winLose();
 };
 
 // Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    //uncomment to hit box
-    //ctx.rect(this.x + 3, this.y + 77,96,67);
-    //ctx.stroke();
 };
 
 //Main Player Char
 var Player = function() {
+    this.x = 200;
+    this.y = 322;
+    this.left = this.x + 17;
+    this.right = this.x + 83;
+    this.top = this.y + 70;
+    this.bottom = this.y + 138;
     this.sprite = 'images/char-boy.png';
-    this.win = 0;
-    this.lose = 0;
+    this.winCount = 0;
+    this.loseCount = 0;
+};
+
+Player.prototype.update = function() {
     this.resetBox = function() {
         this.left = this.x + 17;
         this.right = this.x + 83;
@@ -98,39 +100,60 @@ var Player = function() {
         this.y = 322;
         this.resetBox();
     };
-    this.resetPos();
-};
-
-Player.prototype.update = function() {
-    //////Nothing here!!
+    this.win = function() {
+        if (player.y < 50) {
+            console.log("You Win!!"); //Debug Only
+            //Add to win score and post to html tag
+            player.winCount++;
+            winning.innerHTML = player.winCount;
+            player.resetPos();
+        }
+    };
+    this.lose = function() {
+        //Test for gap between player hitbox and allEnemies
+        //Lose Test
+        for(var i = 0; i < allEnemies.length; i++) {
+            if ((player.left < allEnemies[i].right) &&
+                (player.right > allEnemies[i].left) &&
+                (player.top < allEnemies[i].bottom) &&
+                (player.bottom > allEnemies[i].top)) {
+                //Add to lose score and post to html tag
+                player.loseCount++;
+                losing.innerHTML = player.loseCount;
+                player.resetPos();
+            }
+        }
+    };
+       this.lose();
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    //uncomment to see hit box
-    //ctx.rect(this.left, this.top,66,74);
-    //ctx.stroke();
 };
 
 //Modifies Player's X and Y based on keyboard input
 Player.prototype.handleInput = function(test) {
-    if (test == "up") {
-        this.y -= 83;
-    } else if (test == "down") {
-        if (this.y <= 322) {
-            this.y += 83;
-        }
-    } else if (test == "left") {
-        if (this.x >= 25) {
-            this.x -= 25;
-        }
-    } else if (test == "right") {
-        if (this.x < 400) {
-            this.x += 25;
-        }
+    switch (test){
+        case 'up':
+            this.y -= 83;
+            break;
+        case 'down':
+            if (this.y <= 322) {
+                this.y += 83;
+            }
+            break;
+        case 'left':
+            if (this.x >= 25) {
+                this.x -= 25;
+            }
+            break;
+        case 'right':
+            if (this.x < 400) {
+                this.x += 25;
+            }
     }
     this.resetBox();
-    console.log(this.x + " " + this.y); //Debugging Only
+    this.win();
 };
 
 //creating enemies
